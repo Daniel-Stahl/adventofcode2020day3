a4ef4e73f269db22fc1192d83ef13b2ff0b2ae4c
Daniel Stahl <danny.stahl@gmail.com>
2020-12-22 11:03:09 -0800

    initializing project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdventOfCode2020Day3
+//
+//  Created by Daniel Stahl on 12/22/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
